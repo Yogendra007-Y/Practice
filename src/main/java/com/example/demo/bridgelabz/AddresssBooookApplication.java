8e65c71f17d1d1a969bf23928f9c0c60498d19e8
Yogendra <you@example.com>
2021-12-29 18:47:05 +0530

    ADDED : Practice

---
+++ src/main/java/com/example/demo/bridgelabz/AddresssBooookApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.bridgelabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddresssBooookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddresssBooookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/bridgelabz/AddresssBooookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.bridgelabz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddresssBooookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
